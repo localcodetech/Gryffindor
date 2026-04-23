1a0c7152fd9f30019edd7f8c327c3057ef3d9941
localcodetech <rbphraim95@gmail.com>
2026-04-23 16:16:25 +0000

    django

--- richardbenephraim/django_project/config/urls.py
+++ richardbenephraim/django_project/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_restapi project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..api.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
 ]
--- richardbenephraim/django_project/django_restapi/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Product(models.Model):
-    name =  models.CharField(max_length=100)
-    image = models.ImageField(upload_to="/images", null=True, blank=True)
-    price = models.DecimalField(decimal_places=2,max_digits=200)
