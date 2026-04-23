fedc7816f089624a21d23d984ae3a0fa1f18ba52
localcodetech <rbphraim95@gmail.com>
2026-04-23 16:42:35 +0000

    django

--- richardbenephraim/django_project/todo/views.py
+++ richardbenephraim/django_project/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Category, Channel
 
 # Create your views here.
+
+def index(request):
+    content = Channel.objects.all()
+    cat_content = Category.objects.all()
+
+    response = render(request, 'index.html', context={'channels': content})
+
+    return response
